041e7eac88f2bf6966033d58e8d6c039e911cfee
carlos <radix_10339@hotmail.com>
2016-09-06 08:29:08 -0500

    modificacionn de señales y añadido de programa principal

---
+++ app2/insert_data.py
@@ -0,0 +1,16 @@
+from . import models as models2
+from . import forms
+from .signals import preparing_b
+from app1 import models as models1
+
+def main():
+    dict_b_origen = models1.B.objects.values('id', 'nombre', 'a').first()
+    preparing_b.send(object_dict=dict_b_origen)
+    b_objects = models2.B.objects.filter(id=dict_b_origen.get('id'))
+    if not b_objects:
+        form = forms.BForm(dict_b_origen)
+        if form.is_valid():
+            models2.B.objects.get_or_create(**dict_b_origen)
+
+if __name__ == '__main__':
+    main()
--- app2/signals.py
+++ app2/signals.py
@@ -1,11 +1,23 @@
 from django import dispatch
 from django.dispatch import receiver
 from .models import A, B
-from .forms import AForm, BForm
+from app1 import models as app1_models
+from .forms import AForm
 
 preparing_b = dispatch.Signal(providing_args=['object_dict'])
 preparing_a = dispatch.Signal(providing_args=['object_dict'])
 
 @receiver(preparing_b)
-def insert_b_object(sender, object_dict):
-    pass
+def insert_b_object(sender, object_dict, **kwargs):
+    b_objects = B.objects.filter(id=object_dict.get('id'))
+    if not b_objects.count():
+        a_objects = A.objects.filter(id=object_dict.get('a_id'))
+        if not a_objects.count():
+            preparing_a.send({'id': object_dict.get('a_id')})
+
+@receiver(preparing_a)
+def insert_a_object(sender, object_dict, **kwargs):
+    a = app1_models.A.objects.filter(**object_dict).values('id', 'nombre')[0]
+    form = AForm(a)
+    if form.is_valid():
+        A.objects.get_or_create(**form.cleaned_data)
